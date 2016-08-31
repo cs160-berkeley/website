530740143036017fd36dc74145e1c826d780d2a2
Eric Liang <liang.eric24@gmail.com>
2016-08-31 11:57:05 -0700

    improved schedule data structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,64 +22,67 @@ function scrollBarHandler() {
 var schedule = [
   {
     lectureDay: "Aug 25",
-    lectures: [{
+    topics: [{
       name: 'Introduction',
-      slideURL: 'https://drive.google.com/open?id=0B7qiT-cxa1ayTlJCUXN0S2RXbHM',
-      material: [{
-        title: 'Readings',
-        items: [{
-          title: 'The Task-Centered Design Process',
-          url: 'http://www.hcibib.org/tcuid/chap-1.html',
-          html: 'in <em>Task-Centered User Interface Design</em>. Chap 1. Lewis & Rieman'
-        }, {
-          title: 'Personas: Practice and Theory',
-          url: 'http://research.microsoft.com/en-us/um/people/jgrudin/publications/personas/designchapter.pdf',
-          html: 'by John Pruitt and Jonathan Grudin'
-        }, {
-          title: 'Principles of Contextual Inquiry',
-          url: 'files/readings/1/contextual_inquiry.pdf',
-          html: 'Chapter 3 from <em>Contextual Design</em> by Beyer & Holtzblatt'
-        }]
+      slides: 'https://drive.google.com/open?id=0B7qiT-cxa1ayTlJCUXN0S2RXbHM'
+    }],
+    material: [{
+      title: 'Readings',
+      items: [{
+        title: 'The Task-Centered Design Process',
+        url: 'http://www.hcibib.org/tcuid/chap-1.html',
+        html: 'in <em>Task-Centered User Interface Design</em>. Chap 1. Lewis & Rieman'
       }, {
-        title: 'Assignments',
-        items: [{
-          title: 'Reading Response 01',
-          url: 'https://bcourses.berkeley.edu/courses/1454557/assignments/7736970',
-          html: '<span class="due-date">due before class Thurs, Sep 1</span>'
-        }, {
-          title: 'DESIGN 01: Interviewing',
-          url: 'https://bcourses.berkeley.edu/courses/1454557/assignments/7736272',
-          html: '<span class="due-date">due Fri, Sep 2</span>'
-        }]
+        title: 'Personas: Practice and Theory',
+        url: 'http://research.microsoft.com/en-us/um/people/jgrudin/publications/personas/designchapter.pdf',
+        html: 'by John Pruitt and Jonathan Grudin'
+      }, {
+        title: 'Principles of Contextual Inquiry',
+        url: 'files/readings/1/contextual_inquiry.pdf',
+        html: 'Chapter 3 from <em>Contextual Design</em> by Beyer & Holtzblatt'
+      }]
+    }, {
+      title: 'Assignments',
+      items: [{
+        title: 'Reading Response 01',
+        url: 'https://bcourses.berkeley.edu/courses/1454557/assignments/7736970',
+        html: '<span class="due-date">due before class Thurs, Sep 1</span>'
+      }, {
+        title: 'DESIGN 01: Interviewing',
+        url: 'https://bcourses.berkeley.edu/courses/1454557/assignments/7736272',
+        html: '<span class="due-date">due Fri, Sep 2</span>'
       }]
     }]
   }, {
     lectureDay: "Sep 1",
-    lectures: [{
+    topics: [{
       name: 'Storyboards, Scenarios, and Personas',
-      slideURL: '',
-      material: [{
-        title: 'Readings',
-        items: [{
-          title: 'Direct Manipulation Interfaces',
-          url: 'https://bcourses.berkeley.edu/courses/1454557/files/69530499/download?wrap=1',
-          html: 'Chapter 5 from <em>User Centered System Design</em>. Hutchins, Hollan & Norman.'
-        }, {
-          title: 'The Psychopathology of Everyday Things',
-          url: 'https://bcourses.berkeley.edu/courses/1454557/files/69530519/download?wrap=1',
-          html: 'Chapter 1 from <em>The Design of Everyday Things</em> by Don Norman.'
-        }, {
-          title: 'Cognetics and the Locus of Attention',
-          url: 'https://bcourses.berkeley.edu/courses/1454557/files/69530523/download?wrap=1',
-          html: 'Chapter 2 from <em>The Humane Interface</em> by Jef Raskin. <b>(260A students only)</b> '
-        }]
+      slides: ''
+    }, {
+      name: 'Task Analysis & Contextual Inquiry',
+      slides: ''
+    }],
+    material: [{
+      title: 'Readings',
+      items: [{
+        title: 'Direct Manipulation Interfaces',
+        url: 'https://bcourses.berkeley.edu/courses/1454557/files/69530499',
+        html: 'Chapter 5 from <em>User Centered System Design</em>. Hutchins, Hollan & Norman.'
       }, {
-        title: 'Assignments',
-        items: [{
-          title: 'Reading Response 02',
-          url: 'https://bcourses.berkeley.edu/courses/1454557/assignments/7739076',
-          html: '<span class="due-date">due before class Thurs, Sep 8</span>'
-        }]
+        title: 'The Psychopathology of Everyday Things',
+        url: 'https://bcourses.berkeley.edu/courses/1454557/files/69530519',
+        html: 'Chapter 1 from <em>The Design of Everyday Things</em> by Don Norman.'
+      }, {
+        title: 'Cognetics and the Locus of Attention',
+        url: 'https://bcourses.berkeley.edu/courses/1454557/files/69530523',
+        html: 'Chapter 2 from <em>The Humane Interface</em> by Jef Raskin. <b>(260A students only)</b> '
+      }]
+    }, {
+      title: 'Assignments',
+      items: [{
+        title: 'Reading Response 02',
+        url: 'https://bcourses.berkeley.edu/courses/1454557/assignments/7739076',
+        html: '<span class="due-date">due before class Thurs, Sep 8</span>'
       }]
     }]
   }, {
