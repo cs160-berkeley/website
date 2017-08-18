de9e771ac2031d3f0dac0744213f63def06d69f5
Jeremy Warner <jeremywrnr@gmail.com>
2017-08-18 08:18:18 -0700

    making better parse script

--- _data/parse.rb
+++ _data/parse.rb
@@ -4,16 +4,12 @@ require "pp"
 
 csv_sch = CSV.read "./schedule.csv"
 header = csv_sch.shift # first row
-yaml_sch = YAML.load csv_sch.to_s
 
 # zip csv header with result
-full_yaml = yaml_sch.map do |y|
+full_yaml = csv_sch.map do |y|
   row = Hash.new # for this class
   y.each_with_index { |c, i| row[header[i]] = c }
   row
 end
 
-# todo still need some intermediate step to generate yaml
-
-pp full_yaml[0..1]
-puts header
+puts full_yaml.to_yaml
