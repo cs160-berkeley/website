8aafcdbb908c3b1af49d0e331f64046e19095729
Jeremy Warner <jeremywrnr@gmail.com>
2017-08-22 18:31:47 -0700

    update parsing script to handle more fields

--- _data/parse.rb
+++ _data/parse.rb
@@ -2,7 +2,6 @@ require "yaml"
 require "CSV"
 require "pp"
 
-
 class ClassList
   attr_reader :header, :list
   def initialize(csv)
@@ -14,7 +13,7 @@ class ClassList
   def filterDate(fields)
     Hash[fields
       .each_with_index
-      .map { |c, i| c.nil?? nil : [@header[i], c] }
+      .map { |c, i| c.nil?? nil : [@header[i], c.strip] }
       .reject {|hc| hc.nil? || hc[1] =~ /^\s*$/ || hc[1] == " "}
     ]
   end
@@ -24,11 +23,24 @@ class ClassList
     return d
   end
 
+  def parseRead(d)
+    return d if d["Reading"].nil?
+    d["Reading"] = d["Reading"].split("\n").map { |r|
+
+      {
+        :name => r.split(",").first.strip,
+        :link => r.split(",").last.strip,
+      }
+    }.reject { |r| r.nil? || r.empty? }
+    return d
+  end
+
   def generate
     @list
       .map {|f| filterDate f  }
       .reject {|f| f["Class"].nil? }
       .map { |f| parseDate f  }
+      .map { |f| parseRead f  }
   end
 end
 
